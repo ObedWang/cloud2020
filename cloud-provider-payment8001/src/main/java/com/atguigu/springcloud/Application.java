6fd65b14bdfbe03d8c672f029f17d6c7cb406c4b
wangeb <wangeb@data4truth.com>
2020-10-14 18:36:28 +0800

    init project

---
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Application.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+
+/**
+ * @author : wangeb
+ * @date : 2020/10/14 12:58
+ */
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
