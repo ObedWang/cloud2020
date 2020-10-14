e3ca999c2ba266881a1d0b7b3b8d4e67590561bb
wangeb <wangeb@data4truth.com>
2020-10-14 18:45:56 +0800

    init project

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Application.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/Application.java
@@ -1,12 +1,15 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author : wangeb
  * @date : 2020/10/14 12:58
  */
-
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
