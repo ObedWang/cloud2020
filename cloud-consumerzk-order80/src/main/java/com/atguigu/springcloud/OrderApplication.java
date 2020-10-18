bc14736f1b8ff7628396e007dd872fe4f8096e4b
ObedWang <527488549@qq.com>
2020-10-18 23:59:02 +0800

    zookeeper study

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author ：web
+ * @date ：23:53 2020/10/18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
