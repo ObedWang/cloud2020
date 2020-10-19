364e0d68fdada193b26e3acd75af24527ab505da
wangeb <wangeb@data4truth.com>
2020-10-19 10:26:24 +0800

    consul study

---
+++ cloud-consumerconsul-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+/**
+ * @author ：web
+ * @date ：23:54 2020/10/18
+ */
+@RestController
+@Slf4j
+public class OrderController {
+    public static final String INVOME_URL = "http://consul-provider-payment";
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/consul")
+    public String payment() {
+        String result = restTemplate.getForObject(INVOME_URL + "/payment/consul", String.class);
+        return result;
+    }
+
+}
